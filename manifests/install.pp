c3758ebce12224047132b2bbaa1e5f740d53aac8
Petri Lammi <petri.lammi@tietoteema.fi>
2017-04-07 19:13:51 +0300

    Updates...
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,22 +6,34 @@ class odoo::install inherits odoo::params {
 
   include stdlib
 
+  group { 'Odoo group':
+    ensure      => present,
+    name        => $::odoo::params::odoo_group,
+  }
+
+  user { 'Odoo user':
+    ensure  => present,
+    name    => $::odoo::odoo_user,
+    require => Group['Odoo group'] 
+  }
+  
   file { 'Odoo base path':
-    ensure => directory,
-    path   => $::odoo::params::install_path,
-    owner  => $::odoo::odoo_user,
-    mode   => '0644',
+    ensure      => directory,
+    path        => $::odoo::params::install_path,
+    owner       => $::odoo::odoo_user,
+    mode        => '0644',
+    require => User['Odoo user'] 
   }
 
   vcsrepo { $::odoo::params::install_path:
-    ensure   => present,
-    provider => git,
-    source   => $::odoo::params::odoo_repo_url,
-    revision => $::odoo::params::branch,
-    identity => $::odoo::params::gitsshkey,
-    user     => $::odoo::params::odoo_repouser,
-    depth    => '1',
-    require  => File['Odoo base path'],
+    ensure      => present,
+    provider    => git,
+    source      => $::odoo::params::odoo_repo_url,
+    revision    => $::odoo::params::branch,
+    identity    => $::odoo::params::gitsshkey,
+    user        => $::odoo::params::odoo_repouser,
+    depth       => '1',
+    require     => File['Odoo base path'],
   }
 
   ensure_packages($odoo::dependency_packages)
